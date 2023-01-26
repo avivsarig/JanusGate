e884eab390688703e1bd1c90f6f41f104215d65f
avivsarig <avivs15@gmail.com>
2023-01-26 11:08:50 +0200

    Update code to generate multiple commit messages

--- chat.py
+++ chat.py
@@ -8,7 +8,12 @@ def generate_text(prompt):
     openai.api_key = config["ChatGPT_API_KEY"]
     response_text = openai.Completion.create(
         model="text-davinci-003",
-        prompt=prompt
+        prompt=prompt,
+        n=3,
+        temperature=1
         )
 
-    return response_text['choices'][0]['text'].strip()
+    commit_messages = []
+    for response in response_text['choices']:
+        commit_messages.append(response['text'].strip())
+    return commit_messages
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from diff_prompt import diff_to_str
 from chat import generate_text
 
-diff = diff_to_str()
-prompt = f"Return commit message for this git diff, without any other text:\n\n{diff}"
-commit_message = generate_text(prompt)
-print(commit_message)
+diff: str = diff_to_str()
+prompt: str = f"Return commit message for this git diff, without any other text:\n\n{diff}"
+
+commit_messages: list = generate_text(prompt)
+while len(commit_messages) < 3:
+    more_messages: list = generate_text(prompt)
+    for message in more_messages:
+        commit_messages.add(message)
+
+for message in commit_messages:
+    print(message)
